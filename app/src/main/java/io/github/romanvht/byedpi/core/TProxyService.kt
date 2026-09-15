f7ee5a4e06d612c096f1f3add2732b04a8776754
Tim <Timotods@gmail.com>
2026-09-15 18:43:18 +0300

    upd

--- app/src/main/java/io/github/romanvht/byedpi/core/TProxyService.kt
+++ app/src/main/java/io/github/romanvht/byedpi/core/TProxyService.kt
@@ -1,8 +1,10 @@
 package io.github.romanvht.byedpi.core
 
 object TProxyService {
-    external fun TProxyStartService(configPath: String, fd: Int): Boolean
-    external fun TProxyStopService(): Boolean
+    external fun TProxyStartService(configPath: String, fd: Int)
+    external fun TProxyStopService()
+    external fun TProxyIsRunning(): Boolean
+    external fun TProxyGetStats(): LongArray
 
     init {
         System.loadLibrary("hev-socks5-tunnel")
